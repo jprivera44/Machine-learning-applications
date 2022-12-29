0ca7bdc5dddd0ccede6784840ff75acb8a3b150a
jprivera44 <jprivera44@gmail.com>
2022-12-29 12:52:03 -0600

    adding in rough draft of code

--- NLP/Chatbots/my_chatbot_backend/server.js
+++ NLP/Chatbots/my_chatbot_backend/server.js
@@ -8,11 +8,19 @@ const server = fastify({
   logger: true
 });
 
+//new route that exposes an API endpoint for the react front end located in my_chatbot_frontend to call
+server.get('messages/api',(request,reply) => {
 
+  reply.send({message: 'Hello, mars!'});
+
+});
+
+//The root route that returns a JSON object with a message property
 server.get('/', (request, reply) => {
   reply.send({ message: 'Hello, world!' });
 });
 
+//Running the server on port 3000
 server.listen(3000, (err, address) => {
   if (err) throw err;
   console.log(`Server listening on ${address}`);
